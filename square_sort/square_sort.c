d71cd781e357ad98212f0ab75a8ff86247a6bb0a
zhengxiaoxu <zheng_xiaoxu@qq.com>
2012-08-18 16:13:19 +0800

    add square_sort project

---
+++ square_sort/square_sort.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  square_sort.c
+ *
+ *    Description:  the c file of square sort
+ *
+ *        Version:  1.0
+ *        Created:  08/18/2012 04:12:20 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  zhengxiaoxu (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
---
+++ square_sort/square_sort.h
@@ -0,0 +1,18 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  square_sort.h
+ *
+ *    Description:  some definition of square_sort head
+ *
+ *        Version:  1.0
+ *        Created:  08/18/2012 04:11:11 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  zhengxiaoxu (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
